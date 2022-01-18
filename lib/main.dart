5af7c9086bbea60fd66124fb3c5fa1c399b83470
krittapan <krittapan.msk@gmail.com>
2022-01-18 14:48:35 +0700

    link Authentication Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:kmrs/providers/user_provider.dart';
 import 'package:kmrs/screen/login/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:kmrs/utils/colors.dart';
+import 'package:provider/provider.dart';
+import 'package:kmrs/responsive/responsive_layout.dart';
+import 'package:kmrs/responsive/web_screen_layout.dart';
+import 'package:kmrs/responsive/mobile_screen_layout.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyD145tc1lK8ve_fDRgY9GHLfc5DRp_GiOk",
+          appId: "1:678552025358:web:f55485255cccff9b677f5f",
+          messagingSenderId: "G-XJGWMKJ71G",
+          projectId: "knowleage-management",
+          storageBucket: 'knowleage-management.appspot.com'),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -9,12 +33,45 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'KMRS',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'KMRS',
+        theme: ThemeData(),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.active) {
+              // Checking if the snapshot has any data or not
+              if (snapshot.hasData) {
+                // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                return const ResponsiveLayout(
+                  mobileScreenLayout: MobileScreenLayout(),
+                  webScreenLayout: WebScreenLayout(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              }
+            }
+
+            // means connection to future hasnt been made yet
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return const LoginScreen();
+          },
+        ),
       ),
-      home: LoginScreen(),
     );
   }
 }
--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kmrs/resources/auth_methods.dart';
+import 'package:kmrs/responsive/mobile_screen_layout.dart';
+import 'package:kmrs/responsive/responsive_layout.dart';
+import 'package:kmrs/responsive/web_screen_layout.dart';
+import 'package:kmrs/utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,133 +13,174 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().loginUser(
+        email: _emailController.text, password: _passwordController.text);
+    if (res == 'success') {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (context) => const ResponsiveLayout(
+              mobileScreenLayout: MobileScreenLayout(),
+              webScreenLayout: WebScreenLayout(),
+            ),
+          ),
+          (route) => false);
+
+      setState(() {
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      showSnackBar(context, res);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     print(size.height);
     print(size.width);
     return Scaffold(
-      body: Container(
-        width: size.width,
-        height: size.height,
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                height: double.infinity,
-                width: size.width * 0.6,
-                child: Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: const [
-                      Text(
-                        "ระบบรายงานการจัดการความรู้ภายในส่วนงาน",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
+      body: SafeArea(
+        child: Container(
+          width: size.width,
+          height: size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  height: double.infinity,
+                  width: size.width * 0.6,
+                  child: Padding(
+                    padding: const EdgeInsets.all(32),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: const [
+                        Text(
+                          "ระบบรายงานการจัดการความรู้ภายในส่วนงาน",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "ส่วนงานการจัดการความรู้ภายใน",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
+                        Text(
+                          "ส่วนงานการจัดการความรู้ภายใน",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "มหาวิทยาลัยเกษตรศาสตร",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
+                        Text(
+                          "มหาวิทยาลัยเกษตรศาสตร",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: size.width < 790
-                  ? EdgeInsets.all(size.height < 770
-                      ? 64
-                      : size.height > 670
-                          ? 32
-                          : 16)
-                  : EdgeInsets.fromLTRB(size.width * 0.65, size.height * 0.15,
-                      size.width * 0.05, size.height * 0.15),
-              child: Center(
-                child: Card(
-                  elevation: 4,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(25),
-                    ),
-                  ),
-                  child: Center(
-                    child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(40),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "ลงชื่อเข้าใช้งาน",
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Container(
-                              width: 30,
-                              child: const Divider(
-                                color: Colors.green,
-                                thickness: 2,
-                              ),
-                            ),
-                            const SizedBox(height: 32),
-                            const TextField(
-                              decoration: InputDecoration(
-                                hintText: 'ชื่อผู้ใช้',
-                                labelText: 'ชื่อผู้ใช้',
-                                suffixIcon: Icon(
-                                  Icons.mail_outline,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 32),
-                            const TextField(
-                              decoration: InputDecoration(
-                                hintText: 'รหัสผ่าน',
-                                labelText: 'รหัสผ่าน',
-                                suffixIcon: Icon(
-                                  Icons.lock_outline,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 64),
-                            ElevatedButton(
-                                onPressed: () {},
-                                child: const Text('เข้าสู่ระบบ'),
-                                style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.green))),
-                          ],
-                        ),
-                      ),
+                      ],
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: size.width < 790
+                    ? EdgeInsets.all(size.height < 770
+                        ? 64
+                        : size.height > 670
+                            ? 32
+                            : 16)
+                    : EdgeInsets.fromLTRB(size.width * 0.65, size.height * 0.15,
+                        size.width * 0.05, size.height * 0.15),
+                child: Center(
+                  child: Card(
+                    elevation: 4,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                    ),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(40),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "ลงชื่อเข้าใช้งาน",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Container(
+                                width: 30,
+                                child: const Divider(
+                                  color: Colors.green,
+                                  thickness: 2,
+                                ),
+                              ),
+                              const SizedBox(height: 32),
+                              TextField(
+                                controller: _emailController,
+                                decoration: const InputDecoration(
+                                  hintText: 'ชื่อผู้ใช้',
+                                  labelText: 'ชื่อผู้ใช้',
+                                  suffixIcon: Icon(
+                                    Icons.mail_outline,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 32),
+                              TextField(
+                                controller: _passwordController,
+                                decoration: const InputDecoration(
+                                  hintText: 'รหัสผ่าน',
+                                  labelText: 'รหัสผ่าน',
+                                  suffixIcon: Icon(
+                                    Icons.lock_outline,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 64),
+                              ElevatedButton(
+                                  onPressed: () {loginUser();},
+                                  child: const Text('เข้าสู่ระบบ'),
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              Colors.green))),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: Container(
